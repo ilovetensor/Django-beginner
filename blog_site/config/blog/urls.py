2e8ca836c70d5379912fd7232274087ba509c8d6
ilovetensor <rohanshar11@gmail.com>
2022-12-29 20:11:40 +0530

    Adding Update and Delete Feature

--- blog_site/config/blog/urls.py
+++ blog_site/config/blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from .views import HomePageBlogs, DetailsPage, CreatePost
+from .views import (
+    HomePageBlogs, 
+    DetailsPage, 
+    CreatePost,
+    UpdatePost,
+    DeletePost
+)
 
 urlpatterns = [
     path('', HomePageBlogs.as_view(), name='home'),
     path('details/<int:pk>', DetailsPage.as_view(), name='details'),
     path('create_post', CreatePost.as_view(), name='post_new'),
+    path('update_post/<int:pk>', UpdatePost.as_view(), name='post_update'),
+    path('delete_post/<int:pk>', DeletePost.as_view(), name='post_delete'),
 ]
--- blog_site/config/blog/views.py
+++ blog_site/config/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 # Create your views here.
 
@@ -21,3 +21,13 @@ class CreatePost(CreateView):
     model = Post
     template_name = 'create.html'
     fields = ['title', 'author', 'body']
+
+class UpdatePost(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name='update_post.html'
+
+class DeletePost(DeleteView):
+    model = Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('home')
